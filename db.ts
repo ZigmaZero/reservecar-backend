84615100ec3ee10e0f283f2598f3a403c27acc88
ZigmaZero <wachirawichkanil@gmail.com>
2025-06-24 03:43:06 +0000

    separate test database with dev database

--- db.ts
+++ db.ts
@@ -2,7 +2,9 @@ import Database from 'better-sqlite3';
 import logger from './logger.js';
 
 const db = new Database(
-  'database/database.db',
+  (process.env.NODE_ENV === 'test') 
+  ? 'test_db/database.db' 
+  : 'database/database.db',
   { verbose: (msg) => logger.info(msg) }
 );
 db.pragma('journal_mode = WAL');
